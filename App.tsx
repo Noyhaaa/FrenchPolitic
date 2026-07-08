fe2c5848baaf37ec05266e41bc850fbc0ac976dc
Noyhaaa <yohansti@outlook.fr>
2026-07-08 18:39:41 +0200

    package dependency

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>FrenchPolitic</Text>
+        <Text style={styles.subtitle}>App about politics</Text>
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: '700',
+  },
+  subtitle: {
+    marginTop: 8,
+    fontSize: 16,
+    color: '#666',
+  },
+});
